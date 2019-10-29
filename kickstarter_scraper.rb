292e39cb3ac9fd72e3176b6a0e9c525487dd2578
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-10-29 19:42:59 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,6 +13,9 @@ open fixtures/kickstarter.html
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  projects = {}
+  
+  
+  
 end
 # projects: kickstarter.css("li.project.grid_4")
